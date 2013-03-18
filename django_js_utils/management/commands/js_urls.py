aceb2288c420bf8b32efaf02ee6175011b5aa01c
Barun Saha <barun.saha04@gmail.com>
2013-03-18 13:04:29 +0530

    Exclude admin and table edit URLs
    Exclude the table edit URLs (e.g., app_<table>_delete) from the generated JS files. Admin URLs are also skipped. The resulting JS file will contain only the "app-specific" URLs.

--- django_js_utils/management/commands/js_urls.py
+++ django_js_utils/management/commands/js_urls.py
@@ -9,11 +9,42 @@ from django.conf import settings as project_settings
 
 from django_js_utils import settings as app_settings
 
+from django.db import models
+import re
+
 
 RE_KWARG = re.compile(r"(\(\?P\<(.*?)\>.*?\))") #Pattern for recongnizing named parameters in urls
 RE_ARG = re.compile(r"(\(.*?\))") #Pattern for recognizing unnamed url parameters
     
+# If you access your app with some prefix; blank otherwise
+_PREFIX_ = '/app_name/'
 
+
+# List of apps used in this project -- used to filter the URLs generated in the JS file
+APPS_LIST = ('app1', 'app2',)
+FILTER_PATTERNS = []
+
+# Compile the URL patterns to be excluded
+for app in APPS_LIST:
+    FILTER_PATTERNS.append(re.compile(r'^%s_.*_add$' % app))
+    FILTER_PATTERNS.append(re.compile(r'^%s_.*_history$' % app))
+    FILTER_PATTERNS.append(re.compile(r'^%s_.*_delete$' % app))
+    FILTER_PATTERNS.append(re.compile(r'^%s_.*_change$' % app))
+    FILTER_PATTERNS.append(re.compile(r'^%s_.*_changelist$' % app))
+
+# Some admin URLs to skip
+FILTER_PATTERNS.append(re.compile(r'^app_list$'))
+FILTER_PATTERNS.append(re.compile(r'^logout$'))
+FILTER_PATTERNS.append(re.compile(r'^jsi18n$'))
+FILTER_PATTERNS.append(re.compile(r'^password_change$'))
+FILTER_PATTERNS.append(re.compile(r'^password_change_done$'))
+
+for label in ['user', 'group']:
+    for action in ['add', 'change', 'changelist', 'delete', 'history']:
+        FILTER_PATTERNS.append(re.compile(r'^auth_%s_%s$' % (label, action,)))
+    
+
+    
 class Command(BaseCommand):
     def handle(self, *args, **options):
         """
@@ -25,7 +56,7 @@ class Command(BaseCommand):
         #output to the file
         urls_file = open(app_settings.URLS_JS_GENERATED_FILE, "w")
         urls_file.write("dutils.conf.urls = ")
-        simplejson.dump(js_patterns, urls_file)
+        simplejson.dump(js_patterns, urls_file, separators = (',\n', ':'))
         print "Done generating Javascript urls file %s" % app_settings.URLS_JS_GENERATED_FILE
     
     @staticmethod
@@ -59,7 +90,18 @@ class Command(BaseCommand):
                     if args_matches:
                         for el in args_matches:
                             full_url = full_url.replace(el, "<>")#replace by a empty parameter name
-                    js_patterns[pattern.name] = "/" + full_url
+                    # Changed by Barun
+                    # js_patterns[pattern.name] = "/" + full_url
+                    skip_this_url = False
+                    for filter_pattern in FILTER_PATTERNS:
+                        if filter_pattern.match(pattern.name):
+                            skip_this_url = True
+                            break
+
+                    if not skip_this_url:
+                        print pattern.name
+                        js_patterns[pattern.name] = _PREFIX_ + full_url
+                    # End change
             elif issubclass(pattern.__class__, RegexURLResolver):
                 if pattern.urlconf_name:
                     Command.handle_url_module(js_patterns, pattern.urlconf_name, prefix=pattern.regex.pattern)
